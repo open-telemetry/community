918e082a9ed575a9d5dc4939475eb41560740671
svrnm <sneumann@causely.ai>
2025-08-28 12:16:44 +0200

    add anchor links to SIGs
    Signed-off-by: svrnm <sneumann@causely.ai>

--- scripts/update-sig-tables.py
+++ scripts/update-sig-tables.py
@@ -96,9 +96,9 @@ for group in data:
             calendar = f"[{invites}](https://groups.google.com/a/opentelemetry.io/g/{invites})"
         
         if group_name == "Specification SIGs":
-            markdown_content += f"| <a id=\"{short_name}\"></a>{name} | {meeting} | {notes} | {chats} | {calendar} | {tc_sponsors} | {gc_liaison} | \n"
+            markdown_content += f"| {name}<a id=\"{short_name}\" href=\"#{short_name}\"><sup>#</sup></a> | {meeting} | {notes} | {chats} | {calendar} | {tc_sponsors} | {gc_liaison} | \n"
         else:
-            markdown_content += f"| <a id=\"{short_name}\"></a>{name} | {meeting} | {notes} | {chats} | {calendar} | {gc_liaison} |\n"
+            markdown_content += f"| {name}<a id=\"{short_name}\" href=\"#{short_name}\"><sup>#</sup></a> | {meeting} | {notes} | {chats} | {calendar} | {gc_liaison} |\n"
 
     # Add a newline for spacing after the table
     markdown_content += "\n"
