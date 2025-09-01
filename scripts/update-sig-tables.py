8e69a1a6634321f114cf9284a59d85905479ad2c
Severin Neumann <neumanns@cisco.com>
2025-09-01 16:51:06 +0200

    add anchor links to SIGs (#2954)
    * add anchor links to SIGs
    
    Signed-off-by: svrnm <sneumann@causely.ai>
    
    * add to cspell list
    
    Signed-off-by: svrnm <sneumann@causely.ai>
    
    * add non-breaking space and use icon
    
    Signed-off-by: svrnm <severin.neumann@altmuehlnet.de>
    
    * Update scripts/update-sig-tables.py
    
    Co-authored-by: Trask Stalnaker <trask.stalnaker@gmail.com>
    
    * Update scripts/update-sig-tables.py
    
    Co-authored-by: Trask Stalnaker <trask.stalnaker@gmail.com>
    
    ---------
    
    Signed-off-by: svrnm <sneumann@causely.ai>
    Signed-off-by: svrnm <severin.neumann@altmuehlnet.de>
    Co-authored-by: Trask Stalnaker <trask.stalnaker@gmail.com>

--- scripts/update-sig-tables.py
+++ scripts/update-sig-tables.py
@@ -96,9 +96,9 @@ for group in data:
             calendar = f"[{invites}](https://groups.google.com/a/opentelemetry.io/g/{invites})"
         
         if group_name == "Specification SIGs":
-            markdown_content += f"| <a id=\"{short_name}\"></a>{name} | {meeting} | {notes} | {chats} | {calendar} | {tc_sponsors} | {gc_liaison} | \n"
+            markdown_content += f"| {name}&nbsp;<a id=\"{short_name}\" href=\"#{short_name}\"><sup>🔗</sup></a> | {meeting} | {notes} | {chats} | {calendar} | {tc_sponsors} | {gc_liaison} | \n"
         else:
-            markdown_content += f"| <a id=\"{short_name}\"></a>{name} | {meeting} | {notes} | {chats} | {calendar} | {gc_liaison} |\n"
+            markdown_content += f"| {name}&nbsp;<a id=\"{short_name}\" href=\"#{short_name}\"><sup>🔗</sup></a> | {meeting} | {notes} | {chats} | {calendar} | {gc_liaison} |\n"
 
     # Add a newline for spacing after the table
     markdown_content += "\n"
